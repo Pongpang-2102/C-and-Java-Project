848c449bc351c9878c1fafeb023c9af246b06de3
Puriwat Sangrawee <93346102+Pongpang-2102@users.noreply.github.com>
2022-11-30 20:45:07 +0700

    Update c_sharp_basic command.cs

--- c_sharp_basic command.cs
+++ c_sharp_basic command.cs
@@ -1,4 +1,6 @@
-namespace Academy01
+
+// 01. basic Operation
+namespace Basic_Operation
 {
     internal class Program
     {
@@ -46,3 +48,33 @@ namespace Academy01
         }
     }
 }
+
+// 02. try to say hello with input aname and show popop MassageBox warning
+
+
+namespace test_messagebox
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+    
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            //MessageBox.Show("Hello " + txtName.Text);
+            MessageBox.Show("Hello " + txtName.Text , "Popup" , MessageBoxButton.YesNo , MessageBoxImage.Warning);
+        }
+    }
+}
+
+
+
+
+
+
